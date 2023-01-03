a14657baa7e4948748e92360ed34cdff18813062
Aaron Sullivan <aasullivan1618@gmail.com>
2023-01-02 22:09:14 -0800

    Woah, found an explosion of life just messing around

--- src/main.rs
+++ src/main.rs
@@ -5,12 +5,19 @@ use drawille::Canvas;
 use std::{thread, time};
 // use termsize;
 
-const RATE: u64 = 50;
+const RATE: u64 = 0;
+
+// How to start looking for life:
+// * Fitness function -- bigger unique_iterations X Going to fall into local maxima of loops
+//                       Keep a history of snapshots, look for loops. Stop when you re-reach a
+//                       state.
+// * Mate -- A meme is a contiguous group, or a localized grouping with a small amount of space
+// * Mutations are memes placed nearby or randomly, or just random squares in the beginning
 
 fn main() {
     // let termsize::Size { rows, cols } = termsize::get().unwrap();
     // let size: u32 = std::cmp::min(rows, cols) as u32;
-    let size: u32 = 100;
+    let size: u32 = 150;
 
     let canvas = Canvas::new(size, size);
 
@@ -29,14 +36,21 @@ fn main() {
     cells.birth(midpoint + 1, midpoint + 2);
     cells.birth(midpoint + 1, midpoint + 3);
     cells.birth(midpoint + 1, midpoint + 4);
+    cells.birth(midpoint + 5, midpoint + 4);
+    cells.birth(midpoint + 6, midpoint + 4);
+    cells.birth(midpoint + 6, midpoint + 5);
+    cells.birth(midpoint + 7, midpoint + 4);
+    cells.birth(midpoint + 8, midpoint + 4);
 
     let snapshot = Snapshot::new();
 
     let mut game = Game::new(snapshot, size, cells, Some(canvas));
+    // let mut game = Game::new(snapshot, size, cells, None);
 
     loop {
         thread::sleep(time::Duration::from_millis(RATE));
         game.step();
+        println!("{}", game.unique_iterations);
     }
 }
 
